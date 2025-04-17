e2ca6be1dbc9e3d3947bf7c80e5950c8dd0b2365
vdrozd <vladrozd.dev@gmail.com>
2025-04-17 14:22:54 +0200

    feat: enhance minting section with book edition query and update minting logic

--- components/launchpad/minting-section.tsx
+++ components/launchpad/minting-section.tsx
@@ -4,17 +4,20 @@ import {
   fetchCandyMachine,
   mintV2,
 } from '@metaplex-foundation/mpl-candy-machine'
-import { setComputeUnitLimit } from '@metaplex-foundation/mpl-toolbox'
+import {
+  setComputeUnitLimit,
+  setComputeUnitPrice,
+} from '@metaplex-foundation/mpl-toolbox'
 import {
   generateSigner,
   publicKey,
   transactionBuilder,
 } from '@metaplex-foundation/umi'
-import { useCallback, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 
 import { Book } from '@/api/book/types'
+import { useGetBookEditionsQuery } from '@/api/book-edition/get-book-editions'
 import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
 import { useUmi } from '@/hooks/use-umi'
 import { useUserData } from '@/hooks/use-user-data'
 import { buildGuards } from '@/lib/build-guards'
@@ -24,47 +27,55 @@ interface MintingSectionProps {
 }
 
 export function MintingSection({ book }: MintingSectionProps) {
-  const [tokenCount, setTokenCount] = useState(1)
   const [isLoading, setIsLoading] = useState(false)
-  const totalPrice = Number(book.mint?.price) * tokenCount
+  const [isMinting, setIsMinting] = useState(false)
+  const totalPrice = Number(book.mint?.price)
   const umi = useUmi()
 
-  const { isAuthenticated } = useUserData()
+  const { isAuthenticated, user } = useUserData()
+  const { data: editions } = useGetBookEditionsQuery({
+    bookId: book.id,
+    ownerAddress: user?.wallet?.address,
+  })
 
-  const increaseTokenCount = useCallback(() => {
-    if (!book.metrics?.totalSupply) {
-      return
-    }
+  // const increaseTokenCount = useCallback(() => {
+  //   if (!book.metrics?.totalSupply) {
+  //     return
+  //   }
 
-    if (tokenCount < book.metrics?.totalSupply) {
-      setTokenCount(tokenCount + 1)
-    }
-  }, [tokenCount, book.metrics?.totalSupply])
+  //   if (tokenCount < book.metrics?.totalSupply) {
+  //     setTokenCount(tokenCount + 1)
+  //   }
+  // }, [tokenCount, book.metrics?.totalSupply])
 
-  const decreaseTokenCount = useCallback(() => {
-    if (tokenCount > 1) {
-      setTokenCount(Math.max(1, tokenCount - 1))
-    }
-  }, [tokenCount])
+  // const decreaseTokenCount = useCallback(() => {
+  //   if (tokenCount > 1) {
+  //     setTokenCount(Math.max(1, tokenCount - 1))
+  //   }
+  // }, [tokenCount])
 
-  const handleTokenCountChange = useCallback(
-    (e: React.ChangeEvent<HTMLInputElement>) => {
-      if (!book.metrics?.totalSupply) {
-        return
-      }
+  // const handleTokenCountChange = useCallback(
+  //   (e: React.ChangeEvent<HTMLInputElement>) => {
+  //     if (!book.metrics?.totalSupply) {
+  //       return
+  //     }
 
-      const value = +e.target.value.replace(/[^0-9]/g, '')
+  //     const value = +e.target.value.replace(/[^0-9]/g, '')
 
-      if (value > book.metrics?.totalSupply) {
-        setTokenCount(book.metrics?.totalSupply)
-      } else if (value < 1) {
-        setTokenCount(1)
-      } else {
-        setTokenCount(value)
-      }
-    },
-    [book.metrics?.totalSupply],
-  )
+  //     if (value > book.metrics?.totalSupply) {
+  //       setTokenCount(book.metrics?.totalSupply)
+  //     } else if (value < 1) {
+  //       setTokenCount(1)
+  //     } else {
+  //       setTokenCount(value)
+  //     }
+  //   },
+  //   [book.metrics?.totalSupply],
+  // )
+
+  useEffect(() => {
+    setIsMinting(false)
+  }, [editions?.length])
 
   const handleMint = useCallback(async () => {
     if (!umi) {
@@ -103,10 +114,9 @@ export function MintingSection({ book }: MintingSectionProps) {
         mintEndDate: new Date(new Date().getTime() + 1000 * 60 * 60 * 24 * 30),
       })
 
-      console.log('guards', guards)
-
       const transaction = transactionBuilder()
-        .add(setComputeUnitLimit(umi, { units: 2_800_000 }))
+        .add(setComputeUnitLimit(umi, { units: 2_000_000 }))
+        .add(setComputeUnitPrice(umi, { microLamports: 500_000 }))
         .add(
           mintV2(umi, {
             candyMachine: candyMachine.publicKey,
@@ -119,12 +129,10 @@ export function MintingSection({ book }: MintingSectionProps) {
         )
 
       const signature = await transaction.sendAndConfirm(umi)
-      console.log('Signature:', signature.signature)
-      console.log(
-        `Successfully minted ${tokenCount} token ${tokenCount > 1 ? 's' : ''}!`,
-      )
 
-      return signature.signature
+      setIsMinting(true)
+
+      console.log(`Successfully minted!`, signature)
     } catch (error: unknown) {
       console.error(
         `Failed to mint: ${error instanceof Error ? error.message : 'Unknown error'}`,
@@ -138,8 +146,23 @@ export function MintingSection({ book }: MintingSectionProps) {
     book.mint?.price,
     book.collectionAddress,
     book.creator?.wallet?.address,
-    tokenCount,
   ])
+
+  const getMintButtonText = useCallback(() => {
+    if (isMinting) {
+      return <p>Your new copy is on the way!</p>
+    }
+
+    if (editions?.length && editions?.length === 1) {
+      return <p>You already have 1 copy of this eBook</p>
+    }
+
+    if (editions?.length && editions?.length > 1) {
+      return <p>You already have {editions?.length} copies of this eBook</p>
+    }
+
+    return <p>You don't have any copies of this eBook yet</p>
+  }, [editions, isMinting])
 
   if (!isAuthenticated) {
     return (
@@ -151,8 +174,8 @@ export function MintingSection({ book }: MintingSectionProps) {
 
   return (
     <div className="mb-6 bg-gradient-to-r from-blue-50 to-purple-50 p-4 rounded-lg shadow-md">
-      <div className="flex flex-col sm:flex-row items-center justify-between space-y-4 sm:space-y-0 sm:space-x-4">
-        <div className="flex items-center space-x-2">
+      <div className="flex justify-start flex-col sm:flex-row items-center space-y-4 sm:space-y-0 sm:space-x-4">
+        {/* <div className="flex items-center space-x-2">
           <Button
             onClick={decreaseTokenCount}
             className="bg-blue-500 text-white hover:bg-blue-600 rounded-full w-8 h-8 flex items-center justify-center"
@@ -176,7 +199,7 @@ export function MintingSection({ book }: MintingSectionProps) {
           >
             +
           </Button>
-        </div>
+        </div> */}
         <Button
           onClick={handleMint}
           disabled={isLoading}
@@ -184,8 +207,9 @@ export function MintingSection({ book }: MintingSectionProps) {
         >
           {isLoading
             ? 'Minting...'
-            : `Mint ${tokenCount} token${tokenCount > 1 ? 's' : ''} for ${totalPrice.toFixed(2)} SOL`}
+            : `Mint new copy for ${totalPrice.toFixed(2)} SOL`}
         </Button>
+        {getMintButtonText()}
       </div>
     </div>
   )
