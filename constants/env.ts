404b7630840016234578d6932f3cc6801c9faea2
vdrozd <vladrozd.dev@gmail.com>
2025-04-25 10:21:34 +0200

    feat: add RPC_URL constant and update useRpc hook to utilize dynamic RPC endpoint

--- constants/env.ts
+++ constants/env.ts
@@ -3,3 +3,4 @@ export const AUCTION_HOUSE_ADDRESS: string =
   process.env.NEXT_PUBLIC_AUCTION_HOUSE_ADDRESS ?? ''
 export const PLATFORM_FEE_ADDRESS: string =
   process.env.NEXT_PUBLIC_PLATFORM_FEE_ADDRESS ?? ''
+export const RPC_URL: string = process.env.NEXT_PUBLIC_RPC_URL ?? ''
--- hooks/use-rpc.tsx
+++ hooks/use-rpc.tsx
@@ -1,9 +1,11 @@
 import { Connection } from '@solana/web3.js'
 import { useMemo } from 'react'
 
+import { RPC_URL } from '@/constants/env'
+
 export const useRpc = () => {
   const rpc = useMemo(() => {
-    return new Connection('https://api.devnet.solana.com')
+    return new Connection(RPC_URL)
   }, [])
 
   return { rpc }
